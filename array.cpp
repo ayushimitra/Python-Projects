79175d8279adc6c0be74bcb87f7b4d3273058ee7
ayushimitra <121199269+ayushimitra@users.noreply.github.com>
2022-12-22 12:40:21 +0530

    Add files via upload

---
+++ array.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int num[5];
+    cout<<"Enter 5 numbers:-"<<endl;
+    for(int i=0;i<5;i++)
+    {
+        cin>>num[i];
+    }
+    cout<<"The numbers are:";
+    for(int n=0;n<5;n++)
+    {
+        cout<<num[n]<<" ";
+    }
+    return 0;
+}
